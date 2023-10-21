0c4af6a22beeedcddf844bd1768b2782ed334162
efeaizesogie <efeaizesogie@gmail.com>
2023-10-21 08:58:00 +0100

    removed environment

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,7 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { enableProdMode } from '@angular/core';
-
-
-import * as firebase from 'firebase/app';
-
 import { AppModule } from './app/app.module';
-import { environments } from './environments/environments';
-
-if (environments.production) {
-  enableProdMode();
-}
-
-// Initialize Firebase
 
 
-const firebaseConfig = environments.firebaseConfig;
-const app = firebase.initializeApp(firebaseConfig);
 
 
 platformBrowserDynamic().bootstrapModule(AppModule)
